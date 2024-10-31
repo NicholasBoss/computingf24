90b189713de360f7ec07f7fa895c43098a0d3928
Maddie :) <maddiemunch123@gmail.com>
2024-10-30 18:06:14 -0600

    new swe changes

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -28,7 +28,7 @@ teamController.buildcloudsoluation = async function(req, res){
 
 teamController.buildSWE = async function(req, res){
     res.render('teams/swe', {
-        title: 'SWE Its a me', 
+        title: 'Society of Women Engineers', 
         link: 'teams/swe', 
         //maybe where ejs stuff is supposed to go as well??
     })
